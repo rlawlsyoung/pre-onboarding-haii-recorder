198b34c1e5ef216fb09fa18ce445bae2dc82461e
김진영 <jinyoung01099@gmail.com>
2022-12-13 19:00:11 +0900

    [Fix] Play화면 문구 변경

--- src/components/Play/Empty.jsx
+++ src/components/Play/Empty.jsx
@@ -3,18 +3,19 @@ import styled from 'styled-components';
 const Empty = () => {
   return (
     <Section>
-      <div>환영합니다.</div>
-      <div>녹음을 시작해주세요!</div>
+      <p>좌측 상단의 아이콘을 클릭하여</p>
+      <p>녹음을 시작할 수 있습니다.</p>
     </Section>
   );
 };
 
 const Section = styled.section`
-  div {
+  p {
     width: 80%;
     margin: auto;
     margin-top: 4%;
-    font-size: 20px;
+    font-size: 18px;
+    font-weight: 700;
     color: gray;
     text-align: center;
   }
