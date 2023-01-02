11468f4da06a8037f70bc37201fdbe1f96990f22
김진영 <jinyoung01099@gmail.com>
2023-01-02 22:28:17 +0900

    [Design] AudioBar 너비값 css 수정

--- src/components/Header/AudioBar.jsx
+++ src/components/Header/AudioBar.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import { Dialog } from '@mui/material';
 import { FaTrashAlt, FaPlay } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const AudioBar = ({ index, clickedName, listName, isEditing, handlePlay, handleRemove }) => {
   return (
-    <StyledAudioBar clickedName={clickedName} listName={listName}>
-      <div className='date-name'>
-        <span className='date'>{listName.split('|')[0]}</span>
-        <span>{listName.split('|')[1]}</span>
-      </div>
-      {clickedName === listName ? (
-        <div className='playing'>재생 중</div>
-      ) : (
-        <div className='btn-box'>
-          <span value={index} id={listName} onClick={handlePlay}>
-            <FaPlay />
-          </span>
-          {isEditing && (
-            <span value={index} id={listName} onClick={handleRemove}>
-              <FaTrashAlt />
-            </span>
-          )}
+    <StyledAudioBar className='flex-center' clickedName={clickedName} listName={listName}>
+      <div className='audio-bar-wrapper'>
+        <div className='date-name'>
+          <span className='date'>{listName.split('|')[0]}</span>
+          <span>{listName.split('|')[1]}</span>
         </div>
-      )}
+        {clickedName === listName ? (
+          <div className='playing'>재생 중</div>
+        ) : (
+          <div className='btn-box'>
+            <span value={index} id={listName} onClick={handlePlay}>
+              <FaPlay />
+            </span>
+            {isEditing && (
+              <span value={index} id={listName} onClick={handleRemove}>
+                <FaTrashAlt />
+              </span>
+            )}
+          </div>
+        )}
+      </div>
     </StyledAudioBar>
   );
 };
 
 const StyledAudioBar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   height: 75px;
   padding: 0 10px;
   background-color: ${({ clickedName, listName }) => clickedName == listName && '#efefef'};
@@ -43,35 +41,42 @@ const StyledAudioBar = styled.div`
     box-shadow: 0 0 11px rgba(33, 33, 33, 0.2);
   }
 
-  .date-name {
+  .audio-bar-wrapper {
     display: flex;
-    flex-direction: column;
     align-items: center;
-    justify-content: center;
-    margin-left: 15px;
+    justify-content: space-between;
+    width: 768px;
 
-    .date {
-      font-weight: 700;
+    .date-name {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-left: 15px;
+
+      .date {
+        font-weight: 700;
+      }
+
+      span {
+        margin: 4px 0;
+      }
     }
 
-    span {
-      margin: 4px 0;
+    .btn-box {
+      width: 100px;
+      span {
+        margin: 0 15px;
+        font-size: 20px;
+        cursor: pointer;
+      }
     }
-  }
-
-  .btn-box {
-    width: 100px;
-    span {
+    .playing {
+      width: 70px;
       margin: 0 15px;
       font-size: 20px;
-      cursor: pointer;
+      font-weight: 700;
     }
-  }
-  .playing {
-    width: 70px;
-    margin: 0 15px;
-    font-size: 20px;
-    font-weight: 700;
   }
 `;
 
